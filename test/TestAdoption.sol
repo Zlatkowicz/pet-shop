2b340462c1a3e93244e7eef44cd8ea31110751c9
Armando Zlatkowicz <armandozlatkowicz@gmail.com>
2022-07-26 11:20:17 -0300

    Test two

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -5,13 +5,19 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/Adoption.sol";
 
 contract TestAdoption {
-  // The address of the adoption contract to be tested
-  Adoption adoption = Adoption(DeployedAddresses.Adoption());
+    // The address of the adoption contract to be tested
+    Adoption adoption = Adoption(DeployedAddresses.Adoption());
 
-  // The id of the pet that will be used for testing
-  uint expectedPetId = 8;
+    // The id of the pet that will be used for testing
+    uint expectedPetId = 8;
 
-  // The expected owner of adopted pet is this contract
-  address expectedAdopter = address(this);
+    // The expected owner of adopted pet is this contract
+    address expectedAdopter = address(this);
 
+    // Testing the adopt() function
+    function testUserCanAdoptPet() public {
+	uint returnedId = adoption.adopt(expectedPetId);
+
+	Assert.equal(returnedId, expectedPetId, "Adoption of the expected pet should match what is returned.");
+    }
 }
